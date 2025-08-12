7cbad6617e66dd297d6d0c4d79190119b574e99e
justarandomgeek <justarandomgeek@gmail.com>
2025-08-12 00:08:50 -0400

    meld

--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -1,131 +1,117 @@
 local util = require("util")
-local p = table.deepcopy(data.raw["constant-combinator"]["constant-combinator"])
-p.name = "pushbutton"
-p.minable.result = "pushbutton"
-
-p.pulse_duration = 1
-
-p.icon = "__pushbutton__/graphics/pushbutton-icon.png"
-p.icon_size = 64
-p.sprites =
-  make_4way_animation_from_spritesheet({ layers =
-    {
-      {
-        filename = "__pushbutton__/graphics/pushbutton.png",
+local meld = require("meld")
+local activity_led_light_offset = {0, 0}
+data:extend{
+  meld.meld(table.deepcopy(data.raw["constant-combinator"]["constant-combinator"]), {
+    name = "pushbutton",
+    minable = { result = "pushbutton" },
+    pulse_duration = 1,
+    icon = "__pushbutton__/graphics/pushbutton-icon.png",
+    icon_size = 64,
+    sprites = meld.overwrite(make_4way_animation_from_spritesheet({
+      layers = {
+        {
+          filename = "__pushbutton__/graphics/pushbutton.png",
+          width = 64,
+          height = 64,
+          shift = util.by_pixel(0, 0),
+          scale = 0.5,
+        },
+        {
+          filename = "__pushbutton__/graphics/pushbuttonsh.png",
+          width = 96,
+          height = 64,
+          shift = util.by_pixel(8, 0),
+          draw_as_shadow = true,
+          scale = 0.5,
+        },
+      }
+    })),
+    activity_led_sprites = meld.overwrite({
+      north = {
+        filename = "__pushbutton__/graphics/buttonled.1.png",
         width = 64,
         height = 64,
-        shift = util.by_pixel(0, 0),
+        draw_as_glow = true,
         scale = 0.5,
+      },
+      east = {
+        filename = "__pushbutton__/graphics/buttonled.2.png",
+        width = 64,
+        height = 64,
+        draw_as_glow = true,
+        scale = 0.5,
+      },
+      south = {
+        filename = "__pushbutton__/graphics/buttonled.3.png",
+        width = 64,
+        height = 64,
+        draw_as_glow = true,
+        scale = 0.5,
+      },
+      west = {
+        filename = "__pushbutton__/graphics/buttonled.4.png",
+        width = 64,
+        height = 64,
+        draw_as_glow = true,
+        scale = 0.5,
+      },
+    }),
+    activity_led_light_offsets = meld.overwrite({
+      activity_led_light_offset,
+      activity_led_light_offset,
+      activity_led_light_offset,
+      activity_led_light_offset,
+    }),
+    circuit_wire_connection_points = meld.overwrite({
+      {
+        shadow =
+        {
+          red = util.by_pixel_hr(41, -6),
+          green = util.by_pixel_hr(20, -6)
+        },
+        wire =
+        {
+          red = util.by_pixel_hr(10, -27),
+          green = util.by_pixel_hr(-10, -27)
+        }
       },
       {
-        filename = "__pushbutton__/graphics/pushbuttonsh.png",
-        width = 96,
-        height = 64,
-        shift = util.by_pixel(8, 0),
-        draw_as_shadow = true,
-        scale = 0.5,
+        shadow =
+        {
+          red = util.by_pixel_hr(50, 16),
+          green = util.by_pixel_hr(50, 2)
+        },
+        wire =
+        {
+          red = util.by_pixel_hr(22, -4),
+          green = util.by_pixel_hr(22, -18)
+        }
       },
-    }
-  })
-
-local activity_led_light_offset = {0, 0}
-p.activity_led_sprites =
-{
-  north = {
-    filename = "__pushbutton__/graphics/buttonled.1.png",
-    width = 64,
-    height = 64,
-    draw_as_glow = true,
-    scale = 0.5,
-  },
-  east = {
-    filename = "__pushbutton__/graphics/buttonled.2.png",
-    width = 64,
-    height = 64,
-    draw_as_glow = true,
-    scale = 0.5,
-  },
-  south = {
-    filename = "__pushbutton__/graphics/buttonled.3.png",
-    width = 64,
-    height = 64,
-    draw_as_glow = true,
-    scale = 0.5,
-  },
-  west = {
-    filename = "__pushbutton__/graphics/buttonled.4.png",
-    width = 64,
-    height = 64,
-    draw_as_glow = true,
-    scale = 0.5,
-  },
+      {
+        shadow =
+        {
+          red = util.by_pixel_hr(20, 25),
+          green = util.by_pixel_hr(36, 25)
+        },
+        wire =
+        {
+          red = util.by_pixel_hr(-10, 5),
+          green = util.by_pixel_hr(10, 5)
+        }
+      },
+      {
+        shadow =
+        {
+          red = util.by_pixel_hr(2, -5),
+          green = util.by_pixel_hr(2, 10)
+        },
+        wire =
+        {
+          red = util.by_pixel_hr(-22, -18),
+          green = util.by_pixel_hr(-22, -4)
+        }
+      }
+    })
+  }),
 }
-
-activity_led_light =
-{
-  intensity = 0.8,
-  size = 1,
-}
-
-
-p.activity_led_light_offsets =
-{
-  activity_led_light_offset,
-  activity_led_light_offset,
-  activity_led_light_offset,
-  activity_led_light_offset,
-}
-
-p.circuit_wire_connection_points =
-{
-  {
-    shadow =
-    {
-      red = util.by_pixel_hr(41, -6),
-      green = util.by_pixel_hr(20, -6)
-    },
-    wire =
-    {
-      red = util.by_pixel_hr(10, -27),
-      green = util.by_pixel_hr(-10, -27)
-    }
-  },
-  {
-    shadow =
-    {
-      red = util.by_pixel_hr(50, 16),
-      green = util.by_pixel_hr(50, 2)
-    },
-    wire =
-    {
-      red = util.by_pixel_hr(22, -4),
-      green = util.by_pixel_hr(22, -18)
-    }
-  },
-  {
-    shadow =
-    {
-      red = util.by_pixel_hr(20, 25),
-      green = util.by_pixel_hr(36, 25)
-    },
-    wire =
-    {
-      red = util.by_pixel_hr(-10, 5),
-      green = util.by_pixel_hr(10, 5)
-    }
-  },
-  {
-    shadow =
-    {
-      red = util.by_pixel_hr(2, -5),
-      green = util.by_pixel_hr(2, 10)
-    },
-    wire =
-    {
-      red = util.by_pixel_hr(-22, -18),
-      green = util.by_pixel_hr(-22, -4)
-    }
-  }
-}
-
-data:extend{p}
